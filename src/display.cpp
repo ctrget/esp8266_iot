fd3d7c96ae61036341f917ae5a869174f24cecfb
FantasyGmm <16450052+FantasyGmm@users.noreply.github.com>
2020-03-10 16:22:38 +0800

    删除多余定义

--- src/display.cpp
+++ src/display.cpp
@@ -1,11 +1,8 @@
 #include "esp8266_iot.h"
 #include "display.h"
 
-#define SCL 4
-#define SDA 5
-
-U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/SCL, /* data=*/SDA, /* reset=*/U8X8_PIN_NONE);
-unsigned long dtime = 0;
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
+unsigned long dtime = 0; 
 
 Display::Display()
 {
