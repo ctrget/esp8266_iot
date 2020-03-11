b1d168b67607afb469e9cf18f8962e73a43d0510
crtget <ctrget@gmail.com>
2020-03-12 02:42:24 +0800

    111

--- src/esp8266_iot.ino
+++ src/esp8266_iot.ino
@@ -23,7 +23,7 @@ void btn_click()
     //u8g2.clearBuffer();          // clear the internal memory
     //u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
     //u8g2.drawStr(0, 10, "Hello World!"); // write something to the internal memory
-    //u8g2.sendBuffer();          // transfer internal memory to the display
+    //u8g2.sendBuffer();          // transfer internal memory to the display1
   }
   else
     display.clearDisplay();
